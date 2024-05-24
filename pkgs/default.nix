adf0c0e2ec98909c68caa9f83db4caccd82995af
Tomek Mańko <tomek.manko@railgun-solutions.com>
2024-05-25 00:52:21 +0200

    git-repo: update to 2.45, patch to run locally
    The `repo` tool derivation by default includes only the main wrapper script
    which fetches the actual tool sources from the internet. We modify the
    derivation to provide default local sources patched with support for repo2nix,
    unless specified otherwise with CLI parameters.
    
    Flake compat was updated and nixpkgs-unstable re-introduced to facilitate this
    change.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,13 +1,7 @@
 # SPDX-FileCopyrightText: 2020 Daniel Fullmer and robotnix contributors
 # SPDX-License-Identifier: MIT
 
-{ inputs ? (import (
-    fetchTarball {
-      url = "https://github.com/edolstra/flake-compat/archive/12c64ca55c1014cdc1b16ed5a804aa8576601ff2.tar.gz";
-      sha256 = "0jm6nzb83wa6ai17ly9fzpqc40wg1viib8klq8lby54agpl213w5"; }
-  ) {
-    src =  ../.;
-  }).defaultNix.inputs,
+{ inputs ? (import ../flake/compat.nix).defaultNix.inputs,
   ... }@args:
 
 let
@@ -17,5 +11,5 @@ in nixpkgs.legacyPackages.x86_64-linux.appendOverlays [
     androidPkgs.packages = androidPkgs.packages.x86_64-linux;
     androidPkgs.sdk = androidPkgs.sdk.x86_64-linux;
   })
-  (import ./overlay.nix)
+  (import ./overlay.nix { inherit inputs; })
 ]
