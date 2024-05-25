1120e12d517df3fabc12dd0c36a93e1c0530cdd8
Tomek Mańko <tomek.manko@railgun-solutions.com>
2024-05-25 15:51:20 +0200

    treewide: reformat codebase

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,12 +1,15 @@
 # SPDX-FileCopyrightText: 2020 Daniel Fullmer and robotnix contributors
 # SPDX-License-Identifier: MIT
 
-{ inputs ? (import ../flake/compat.nix).defaultNix.inputs,
-  ... }@args:
+{
+  inputs ? (import ../flake/compat.nix).defaultNix.inputs,
+  ...
+}@args:
 
 let
   inherit (inputs) nixpkgs androidPkgs;
-in nixpkgs.legacyPackages.x86_64-linux.appendOverlays [
+in
+nixpkgs.legacyPackages.x86_64-linux.appendOverlays [
   (self: super: {
     androidPkgs.packages = androidPkgs.packages.x86_64-linux;
     androidPkgs.sdk = androidPkgs.sdk.x86_64-linux;
--- treefmt.nix
+++ treefmt.nix
@@ -2,7 +2,10 @@
 {
   # Adding this options is a workaround for `config.build.programs` not evaluating properly
   # to get all formatter binaries into a devShell
-  options.programs.mypy.package = lib.mkPackageOption pkgs  [ "python3Packages" "mypy" ] { };
+  options.programs.mypy.package = lib.mkPackageOption pkgs [
+    "python3Packages"
+    "mypy"
+  ] { };
   config = {
     projectRootFile = "flake.nix";
 
@@ -11,11 +14,21 @@
       nixpkgs-fmt.package = pkgs.nixfmt-rfc-style;
 
       mypy.enable = true;
-      mypy.package = pkgs.python3.withPackages(ps: with ps; [ mypy pytest ]);
+      mypy.package = pkgs.python3.withPackages (
+        ps: with ps; [
+          mypy
+          pytest
+        ]
+      );
       mypy.directories = {
         "." = {
           # Fot whatever reason using `settings.formatter.mypy.excludes` does not work properly
-          options = [ "--exclude" "apks/chromium" "--exclude" "result" ];
+          options = [
+            "--exclude"
+            "apks/chromium"
+            "--exclude"
+            "result"
+          ];
           # Has to be set again, because treefmt clears PYTHONPATH and running via `treefmt` fails, see:
           # https://github.com/numtide/treefmt-nix/blob/main/programs/mypy.nix#L67
           extraPythonPackages = [ pkgs.python3Packages.pytest ];
